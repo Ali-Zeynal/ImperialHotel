cec5689ff8faa54e567dcc23a218ba6f2c558005
Ali Zeynal <110941257+Ali-Zeynal@users.noreply.github.com>
2024-03-01 01:14:35 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
-const filterMenu = $.querySelector(".filter-menu-btn");
-const filterButton = $.querySelectorAll(".filter-item");
-const roomsCard = $.querySelectorAll(".rooms-card");
-
-filterButton.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const filter = e.target.dataset.filter;
-
-    if (!document.startViewTransition) {
-      updateActiveButton(e.target);
-      filtering(filter);
-    }
-    document.startViewTransition(() => {
-      updateActiveButton(e.target);
-      filtering(filter);
-    });
-  });
+const $ = document;
+const MobileMenuButton = $.querySelector(".nav-toggle-label");
+const MobileMenuCheckbox = $.querySelector("#nav-toggle");
+const toTopBtn = $.querySelector(".toTop-btn");
+let navMenuItem = $.querySelectorAll(".nav-menu li a");
+let menuDarkCover = $.querySelector("#cover");
+MobileMenuButton.addEventListener("click", () => {
+  menuDarkCover.classList.toggle("covering");
+});
+menuDarkCover.addEventListener("click", () => {
+  MobileMenuCheckbox.click();
+  menuDarkCover.classList.toggle("covering");
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 380 && $.documentElement.clientWidth > 600) {
+    toTopBtn.style.opacity = "1";
+  } else {
+    toTopBtn.style.opacity = "0";
+  }
 });
 
-function updateActiveButton(newButton) {
-  filterMenu.querySelector(".active").classList.remove("active");
-  newButton.classList.add("active");
-}
+toTopBtn.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+});
+navMenuItem.forEach((item) => {
+  if (window.location.pathname.slice(15) == item.attributes.href.nodeValue) {
+    item.style.color = "rgb(248, 170, 72)";
+  } else if (!window.location.pathname.slice(15)) {
+    navMenuItem[0].style.color = "rgb(248, 170, 72)";
+  }
+});
 
-function filtering(filterRoom) {
-  roomsCard.forEach((card) => {
-    const cardCategory = card.dataset.card;
-    console.log();
-
-    if (filterRoom === "all" || filterRoom === cardCategory) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
-}
+window.addEventListener("load", function () {
+  $.querySelector(".loader").style.display = "none";
+  $.querySelector("body").style.visibility = "visible";
+});
