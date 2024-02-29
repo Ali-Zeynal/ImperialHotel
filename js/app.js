ecde57cbef5399585bbc2760552711b011457b82
Ali Zeynal <110941257+Ali-Zeynal@users.noreply.github.com>
2024-02-29 12:59:46 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,12 +5,14 @@ let navMenuItem = document.querySelectorAll(".nav-menu li a");
 let menuDarkCover = document.querySelector("#cover");
 
 MobileMenuButton.addEventListener("click", () => {
-  cover.classList.toggle("covering");
-});
-menuDarkCover.addEventListener("click", () => {
   menuDarkCover.classList.toggle("covering");
-  MobileMenuCheckbox.click();
 });
+
+menuDarkCover.addEventListener("click", () => {
+  MobileMenuCheckbox.click();
+  menuDarkCover.classList.toggle("covering");
+});
+
 window.addEventListener("scroll", function () {
   if (window.scrollY > 380 && document.documentElement.clientWidth > 600) {
     toTopBtn.style.opacity = "1";
@@ -24,21 +26,15 @@ toTopBtn.addEventListener("click", function () {
 });
 
 navMenuItem.forEach((item) => {
+  console.log(item.attributes.href.nodeValue);
   if (window.location.pathname.slice(15) == item.attributes.href.nodeValue) {
     item.style.color = "rgb(248, 170, 72)";
-  } else if (!window.location.pathname.slice(15)) {
+  } else if (!window.location.pathname.slice(1)) {
     navMenuItem[0].style.color = "rgb(248, 170, 72)";
   }
 });
 
 window.addEventListener("load", function () {
   document.querySelector(".loader").style.display = "none";
-  document.querySelector("body").style.visibility = "visible";  
+  document.querySelector("body").style.visibility = "visible";
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   setTimeout(() => {
-//     document.querySelector(".loader").style.display = "none";
-//     document.querySelector("body").style.visibility = "visible";
-//   }, 1500);
-// });
